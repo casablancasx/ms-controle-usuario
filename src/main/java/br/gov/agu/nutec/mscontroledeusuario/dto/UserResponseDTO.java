5b0a5b7bebcde314f4bb2cbaa2a84b1f2f347263
DaniloChaves <danchavesds@gmail.com>
2025-10-06 10:10:24 -0300

    Refactor entities to support relationships and services.
    Replaced String fields with entity relationships for `ClasseJudicial` and `Assunto` in `AudienciaEntity`. Added services and repository methods to handle fetching or creating related entities dynamically. Updated dependent classes to accommodate these changes.

--- src/main/java/br/gov/agu/nutec/mscontroledeusuario/dto/UserResponseDTO.java
+++ src/main/java/br/gov/agu/nutec/mscontroledeusuario/dto/UserResponseDTO.java
@@ -1,10 +1,13 @@
 package br.gov.agu.nutec.mscontroledeusuario.dto;
 
 
+import br.gov.agu.nutec.mscontroledeusuario.enums.UserRole;
+
 public record UserResponseDTO(
         Long sapiensId,
         String nome,
         String email,
-        SetorResponse setor
+        SetorResponse setor,
+        UserRole role
 ) {
 }
