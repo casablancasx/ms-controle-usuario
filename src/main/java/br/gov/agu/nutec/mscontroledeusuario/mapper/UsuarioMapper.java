59f757d29d34e8205b5048473c362a973d920d3e
DaniloChaves <danchavesds@gmail.com>
2025-10-06 09:53:11 -0300

    Update ID handling to use Long instead of Integer
    Replaced Integer with Long for IDs across several classes and DTOs to ensure consistency and support larger ID values. Adjusted method signatures, repository interfaces, and query logic accordingly. Removed unnecessary comments in mapping interfaces for better code clarity.

--- src/main/java/br/gov/agu/nutec/mscontroledeusuario/mapper/UsuarioMapper.java
+++ src/main/java/br/gov/agu/nutec/mscontroledeusuario/mapper/UsuarioMapper.java
@@ -14,24 +14,19 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
-    // Usuario -> UserResponseDTO
-    @Mapping(target = "usuarioId", source = "usuarioId")
     @Mapping(target = "nome", source = "nome")
     @Mapping(target = "email", source = "email")
     @Mapping(target = "setor", source = "setor")
     UserResponseDTO mapToResponse(UsuarioEntity usuario);
 
-    // Setor -> SetorResponse
     @Mapping(target = "id", source = "setorId")
     @Mapping(target = "nome", source = "nome")
     @Mapping(target = "unidade", source = "unidade")
     SetorResponse toDto(SetorEntity setor);
 
-    // Unidade -> UnidadeResponse
     @Mapping(target = "id", source = "unidadeId")
     @Mapping(target = "nome", source = "nome")
     UnidadeResponse toDto(UnidadeEntity unidade);
 
-    // List mapping convenience
     List<UserResponseDTO> mapToResponse(List<UsuarioEntity> usuarios);
 }
